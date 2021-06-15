feb5a367e3a5e7add81d8711413e84e1df258d2c
Tatsuya-kanaria <kanaria.git@gmail.com>
2021-06-15 09:14:21 +0900

    rgb変換

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,16 +1,55 @@
 def to_hex(r, g, b)
-  # inject(初期値) 配列をたす
-  [r, g, b].inject('#') do |hex, n|
-    hex + n.to_s(16).rjust(2, '0')
-  end
+  # '#' +
+  # r.to_s(16).rjust(2, '0') +
+  # g.to_s(16).rjust(2, '0') +
+  # b.to_s(16).rjust(2, '0')
 
   # hex = '#'
   # [r, g, b].each do |n|
   #   hex += n.to_s(16).rjust(2, '0')
   # end
 
-  # '#' +
-  # r.to_s(16).rjust(2, '0') +
-  # g.to_s(16).rjust(2, '0') +
-  # b.to_s(16).rjust(2, '0')
+  # inject(初期値) (((# + r) + g ) + b )
+  [r, g, b].inject('#') do |hex, n|
+    hex + n.to_s(16).rjust(2, '0')
+  end
+end
+
+def to_ints(hex)
+  # r = hex[1..2]
+  # g = hex[3..4]
+  # b = hex[5..6]
+  # ints = []
+  # [r, g, b].each do |s|
+  #   ints << s.hex
+  # end
+  # ints
+
+  # mapを使用することでintsが不要になる
+  # r = hex[1..2]
+  # g = hex[3..4]
+  # b = hex[5..6]
+  # [r, g, b].map do |s|
+  #   s.hex
+  # end
+
+  # # 多重代入をする
+  # r, g, b = hex[1..2], hex[3..4], hex[5..6]
+  # [r, g, b].map do |s|
+  #   s.hex
+  # end
+
+  # 正規表現　\w アルファベット、アンダーバー、数字
+  # .scan 正規表現にマッチした文字列を配列にして返す
+  # r, g, b = hex.scan(/\w\w/)
+  # [r, g, b].map do |s|
+  #   s.hex
+  # end
+
+  # 配列を作らなくてもmap使える
+  # hex.scan(/\w\w/).map do |s|
+  #   s.hex
+  # end
+
+  hex.scan(/\w\w/).map(&:hex)
 end
