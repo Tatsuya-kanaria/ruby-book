7325cd5a0dbf4c79c79aa9a629670f8b0f2b3d9c
Tatsuya-kanaria <kanaria.git@gmail.com>
2021-06-19 14:30:35 +0900

    product 修正

--- lib/product.rb
+++ lib/product.rb
@@ -6,6 +6,16 @@ class Product
     @price = price
   end
 
+  def display_text
+    stock = stock? ? 'あり' : 'なし'
+    "商品名：#{@name} 価格：#{@price} 円 在庫：#{stock}"
+  end
+
+  # subclassでstock?を定義してもらう前提で使用している
+  def stock?
+    raise 'Must implement stock? in subclass.'
+  end
+
   def to_s
     "name: #{name}, price: #{price}"
   end
@@ -19,13 +29,20 @@ class DVD < Product
     @running_time = running_time
   end
 
+  # 在庫があればtrueを返す
+  def stock?
+    true
+  end
+
   def to_s
     "#{super}, running_time: #{running_time}"
   end
 end
 
 product = Product.new('q',100)
-puts "Product: #{product.to_s}"
+# puts "Product: #{product.to_s}"
+puts product.display_text
 
 dvd = DVD.new('qqq', 1000, 100)
-puts "DVD: #{dvd.to_s}"
+# puts "DVD: #{dvd.to_s}"
+puts dvd.display_text
